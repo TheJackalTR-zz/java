3024a38fb9e2cd0d5acc79352f13c864f6b1a986
TheJackalTR <aryen.hemvatan@std.yeditepe.edu.tr>
2019-12-09 11:27:51 +0300

    initial commit

---
+++ Car/src/app/Car.java
@@ -0,0 +1,14 @@
+package app;
+
+public class Car {
+
+    int maxSpeed = 100;
+    int minSpeed;
+    double weight;
+    boolean isTheCarOn;
+    char condition;
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello Java");
+    }
+}
